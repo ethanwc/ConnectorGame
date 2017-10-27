053177738afa72a94ca809f7e4a7bab7d1b8725f
ethanwc <ethanwcheatham@gmail.com>
2017-10-27 13:20:08 -0700

    Needs scoring

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,13 @@
+import javafx.beans.value.ObservableValue;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 public class Game extends JFrame {
@@ -7,22 +15,19 @@ public class Game extends JFrame {
     public JPanel panelBoard = new JPanel();
     public JPanel panelBottom = new JPanel();
     public ArrayList<JPanel> panels = new ArrayList<>();
-    public ArrayList<Shapes> shapes = new ArrayList();
-    public int x,y,z;
-    public int barSize;
+    public ArrayList<Line> lines = new ArrayList<>();
+    public ArrayList<Ellipse> circles = new ArrayList<>();
+    public int x,y, barSize, xScale, cScale;
+    public boolean turn;
 
     public Game() {
-
-//        Shapes st = new Shapes();
-//        Shapes.Start test = st.new Start(new Rectangle(0,0,500,500),1,1,Color.ORANGE);
-//        shapes.add(test);
 
         x = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         y = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
-        panelTop.setBackground(Color.red);
+        panelTop.setBackground(Color.GRAY);
         panelBoard.setBackground(Color.BLACK);
-        panelBottom.setBackground(Color.BLUE);
+        panelBottom.setBackground(Color.GRAY);
 
         add(panelBoard, BorderLayout.CENTER);
         add(panelTop, BorderLayout.NORTH);
@@ -31,7 +36,17 @@ public class Game extends JFrame {
 
         for (int i = 0; i < 9; i++) {
             JPanel panel = new JPanel();
-            panel.setBackground(new Color((int)(Math.random() *255),(int)(Math.random() *255),(int)(Math.random() *255)));
+            final int localI = i;
+            panel.addMouseListener(new MouseAdapter() {
+
+                                       @Override
+                                       public void mouseClicked(MouseEvent e) {
+                                           super.mouseClicked(e);
+                                           System.out.println("Press detected on " +localI);
+                                           nextMove(localI);
+                                       }
+                                   });
+                    panel.setBackground(Color.WHITE);
             panels.add(panel);
             panelBoard.add(panels.get(i));
         }
@@ -47,36 +62,69 @@ public class Game extends JFrame {
         pack();
 
         barSize = getHeight() - panelTop.getHeight() - panelBoard.getHeight() - panelBottom.getHeight();
+        xScale = (int) (panels.get(1).getHeight() *.2);
+        cScale = (int) (panels.get(1).getHeight() * .1);
+
+        //TODO Winner Logic
+        //TODO Boolean Turn
+        //TODO Reset Game...end the new Game() and do a new Game?
+
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(16));
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(20));
         g2.setColor(Color.BLACK);
         //draw board
         g2.drawLine(getX1(1),getY2(1),getX1(1),getY1(7));
         g2.drawLine(getX1(2),getY2(2),getX1(2),getY1(8));
-
         g2.drawLine(getX1(0),getY1(0),getX2(2),getY1(0));
         g2.drawLine(getX1(6),getY1(3),getX2(8),getY1(3));
-        g2.setStroke(new BasicStroke(10));
 
 
+        for (int i = 0; i < circles.size(); i++) {
+            g2.setColor(Color.BLACK);
+            BasicStroke temp = new BasicStroke(12, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+            g2.setStroke(temp);
+            g2.drawOval((int)circles.get(i).getCenterX(),(int)circles.get(i).getCenterY(),(int)circles.get(i).getRadiusX(),(int)circles.get(i).getRadiusY());
 
-        for (int i = 0; i < panels.size(); i++) {
-//            g2.setColor(new Color((int)(Math.random() *255),(int)(Math.random() *255),(int)(Math.random() *255)));
-            g2.setColor(Color.white);
-//
 
-            //draws x and os
-            g2.drawLine(getX1(i),getY1(i),getX2(i),getY2(i));
-            g2.drawLine(getX1(i),getY2(i),getX2(i),getY1(i));
-//            g2.drawOval(getX1(i),getY2(i),panels.get(i).getWidth(),panels.get(i).getHeight());
+        }
+
+        for (int i = 0; i < lines.size(); i++) {
+            g2.drawLine((int)lines.get(i).getStartX(),(int)lines.get(i).getStartY(),(int)lines.get(i).getEndX(),(int)lines.get(i).getEndY());
+
         }
 
 
+
     }
+
+    //determines whether to draw x or o
+    public void nextMove(int i) {
+        if (turn == true) {
+            drawX(i);
+        }
+        else drawC(i);
+        turn = !turn;
+    }
+
+    //draws the 'x' piece
+    public void drawX(int i) {
+        lines.add(new Line(getX1(i)+xScale,getY1(i)-xScale,getX2(i)-xScale,getY2(i)+xScale));
+        lines.add(new Line(getX1(i)+xScale,getY2(i)+xScale,getX2(i)-xScale,getY1(i)-xScale));
+        repaint();
+    }
+
+    //draws the 'o' piece
+    public void drawC(int i) {
+        circles.add(new Ellipse(getX1(i)+cScale,getY2(i)+cScale,panels.get(i).getWidth()-cScale*2,panels.get(i).getHeight()-cScale*2));
+        repaint();
+    }
+
+    //Uses the GridLayout to arrange a 3x3 area, these calls are for finding cords of the 9 panels to scale
 
     public int getX1(int i) {
 
@@ -85,7 +133,7 @@ public class Game extends JFrame {
     }
 
     public int getY1 (int i) {
-        int y1 = panels.get(i).getY() + panels.get(i).getHeight() + panelTop.getHeight() + barSize; //frame height?
+        int y1 = panels.get(i).getY() + panels.get(i).getHeight() + panelTop.getHeight() + barSize;
         return y1;
     }
 
@@ -98,8 +146,6 @@ public class Game extends JFrame {
         int y2 = panels.get(i).getY() + panelTop.getHeight() +barSize;
         return y2;
     }
-
-
 
     public static void main(String[] args) {
         new Game();
